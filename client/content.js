e535d3194259d9d8028ffb52c2e00bf95d6de8a7
Andy Shu <andy@symbol.is>
2018-06-09 23:40:10 +0800

    Extract eos config

--- client/content.js
+++ client/content.js
@@ -71,26 +71,11 @@ function main() {
         }
     })
 
-    const config = {
-        chainId: 'cec278a9dced800d9a695adc1e265ed11efa0ad8a70cdaac1eb65718bbe2434f',
-        contractSender: "commitittest",
-        keyProvider: [
-            `5JHeN2n3ai7awp9tWDwe12rV5ia9oLfZkxMs9oLz18FND7EvcjT`
-        ], // WIF string or array of keys..
-        httpEndpoint: 'http://203.195.171.163:8888',
-        // httpEndpoint: 'http://127.0.0.1:8888',
-        // mockTransactions: () => 'pass', // or 'fail'
-        expireInSeconds: 60,
-        broadcast: true,
-        debug: false, // API and transactions
-        sign: true
-    }
-
     const deley = (time) => new Promise((resolve => setTimeout(resolve, time)))
 
     async function handleAppreciation() {
         buttonState.state = 'liking'
-        const eos = Eos(config)
+        const eos = Eos(window.config)
         try {
             const result = await eos.transaction({
                 actions: [
--- server/routine.js
+++ server/routine.js
@@ -1,4 +1,5 @@
 const Eos = require('eosjs')
+const config = require('../client/eosConfig')
 
 setInterval(() => {
     console.info(`Executing routine on ${Date.now()}`)
